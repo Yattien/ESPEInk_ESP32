00c8685ed0ed588bab921ab9a2466ab0e39c05a9
Yattien <36767821+Yattien@users.noreply.github.com>
2021-06-13 22:07:00 +0200

    build script for local (maintainer) builds

---
+++ build.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+ARDUINO_RUNTIME=~/.arduino15
+ARDUINO_HOME=/opt/arduino-1.8.15
+INO_FILE=ESPEInk_ESP32.ino
+LIBRARY_PATH=~/Arduino/libraries
+BOARD_DEFINITION="esp32:esp32:esp32:PSRAM=disabled,PartitionScheme=default,CPUFreq=240,FlashMode=qio,FlashFreq=80,FlashSize=4M,UploadSpeed=921600,DebugLevel=none"
+TOOL_VERSION=1.22.0-97-gc752ad5-5.2.0
+FS_VERSION=${FS_VERSION}
+IDE_VERSION=10815
+ESP_TOO_VERSION=3.0.0
+
+# prepare
+mkdir -p /tmp/ESP32/cache
+
+${ARDUINO_HOME}/arduino-builder \
+	-dump-prefs \
+	-logger=machine \
+	-hardware ${ARDUINO_HOME}/hardware \
+	-hardware ${ARDUINO_RUNTIME}/packages \
+	-tools ${ARDUINO_HOME}/tools-builder \
+	-tools ${ARDUINO_HOME}/hardware/tools/avr \
+	-tools ${ARDUINO_RUNTIME}/packages \
+	-built-in-libraries ${ARDUINO_HOME}/libraries \
+	-libraries ${LIBRARY_PATH} \
+	-fqbn=${BOARD_DEFINITION} \
+	-ide-version=${IDE_VERSION} \
+	-build-path /tmp/ESP32 \
+	-warnings=all \
+	-build-cache /tmp/ESP32/cache \
+	-prefs=build.warn_data_percentage=75 \
+	-prefs=runtime.tools.mkspiffs.path=${ARDUINO_RUNTIME}/packages/esp32/tools/mkspiffs/0.2.3 \
+	-prefs=runtime.tools.mkspiffs-0.2.3.path=${ARDUINO_RUNTIME}/packages/esp32/tools/mkspiffs/0.2.3 \
+	-prefs=runtime.tools.esptool_py.path=${ARDUINO_RUNTIME}/packages/esp32/tools/esptool_py/${ESP_TOO_VERSION} \
+	-prefs=runtime.tools.esptool_py-2.6.1.path=${ARDUINO_RUNTIME}/packages/esp32/tools/esptool_py/${ESP_TOO_VERSION} \
+	-prefs=runtime.tools.xtensa-esp32-elf-gcc.path=${ARDUINO_RUNTIME}/packages/esp32/tools/xtensa-esp32-elf-gcc/${TOOL_VERSION} \
+	-prefs=runtime.tools.xtensa-esp32-elf-gcc-1.22.0-80-g6c4433a-5.2.0.path=${ARDUINO_RUNTIME}/packages/esp32/tools/xtensa-esp32-elf-gcc/${TOOL_VERSION} \
+	-verbose \
+	${INO_FILE}
+
+${ARDUINO_HOME}/arduino-builder \
+	-compile \
+	-logger=machine \
+	-hardware ${ARDUINO_HOME}/hardware \
+	-hardware ${ARDUINO_RUNTIME}/packages \
+	-tools ${ARDUINO_HOME}/tools-builder \
+	-tools ${ARDUINO_HOME}/hardware/tools/avr \
+	-tools ${ARDUINO_RUNTIME}/packages \
+	-built-in-libraries ${ARDUINO_HOME}/libraries \
+	-libraries ${LIBRARY_PATH} \
+	-fqbn=${BOARD_DEFINITION} \
+	-ide-version=${IDE_VERSION} \
+	-build-path /tmp/ESP32 \
+	-warnings=none \
+	-build-cache /tmp/ESP32/cache \
+	-prefs=build.warn_data_percentage=75 \
+	-prefs=runtime.tools.mkspiffs.path=${ARDUINO_RUNTIME}/packages/esp32/tools/mkspiffs/0.2.3 \
+	-prefs=runtime.tools.mkspiffs-0.2.3.path=${ARDUINO_RUNTIME}/packages/esp32/tools/mkspiffs/0.2.3 \
+	-prefs=runtime.tools.esptool_py.path=${ARDUINO_RUNTIME}/packages/esp32/tools/esptool_py/${ESP_TOO_VERSION} \
+	-prefs=runtime.tools.esptool_py-2.6.1.path=${ARDUINO_RUNTIME}/packages/esp32/tools/esptool_py/${ESP_TOO_VERSION} \
+	-prefs=runtime.tools.xtensa-esp32-elf-gcc.path=${ARDUINO_RUNTIME}/packages/esp32/tools/xtensa-esp32-elf-gcc/${TOOL_VERSION} \
+	-prefs=runtime.tools.xtensa-esp32-elf-gcc-1.22.0-80-g6c4433a-5.2.0.path=${ARDUINO_RUNTIME}/packages/esp32/tools/xtensa-esp32-elf-gcc/${TOOL_VERSION} \
+	-verbose \
+	${INO_FILE}
+
+# upload to local firmware server
+scp /tmp/ESP32/ESPEInk_ESP32.ino.bin volker:
+gzip -f /tmp/ESP32/ESPEInk_ESP32.ino.bin
